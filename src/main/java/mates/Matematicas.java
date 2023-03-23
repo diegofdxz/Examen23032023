9a9cd3f4e64b81da1ad653632ae4756e1527374a
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-03-23 19:11:07 +0100

    Initial commit

---
+++ src/main/java/mates/Matematicas.java
@@ -0,0 +1,12 @@
+package mates;
+
+public class Matematicas{
+    /**
+     * Genera una aproximación al número pi mediante el método de
+     * Montecarlo. El parámetro ‘pasos‘ indica el número de puntos
+     * generado.
+     */
+    public static double generarNumeroPi(long pasos){
+        return 0; // Este código hay que cambiarlo.
+    }
+}
