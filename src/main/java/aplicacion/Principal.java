a49b4b1e8983f6785f0e19dcbaef717eabd24818
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-03-23 19:11:22 +0100

    Commit 1

--- src/main/java/aplicacion/Principal.java
+++ src/main/java/aplicacion/Principal.java
@@ -1,2 +1,8 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import mates.Matematicas;
+public class Principal{
+    public static void main(String[] args){
+        System.out.println("El número PI es " + Matematicas.
+                generarNumeroPi(Integer.parseInt(args[0])));
+    }
 }
